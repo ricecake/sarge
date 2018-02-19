9b3855c97ae05b90fee812865ff2041eeaea3ce9
Sebastian Green-Husted <sgreenhusted@liquidweb.com>
2018-02-18 19:55:54 -0500

    get database connection running

--- models/utils.go
+++ models/utils.go
@@ -1,15 +1,32 @@
 package models
 
 import (
-	"github.com/spf13/viper"
-	//"github.com/jinzhu/gorm"
 	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/spf13/viper"
 )
 
-func init() {
-	Connect()
+var db *gorm.DB
+
+func Connect() (*gorm.DB, error) {
+	var err error
+	if db == nil {
+		username := viper.GetString("db.username")
+		password := viper.GetString("db.password")
+		host := viper.GetString("db.host")
+		database := viper.GetString("db.database")
+		db, err = gorm.Open("mysql", fmt.Sprintf("%s:%s@%s/%s?charset=utf8&parseTime=True&loc=Local", username, password, host, database))
+	}
+
+	return db, err
 }
 
-func Connect() error {
-	return nil
+func GetDb() (*gorm.DB) {
+	if db == nil {
+		if _, err := Connect(); err != nil {
+			panic(err)
+		}
+	}
+	return db
 }
