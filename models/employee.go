b91860a56c451ca0c7701e995071cc9f4e939b06
Sebastian Green-Husted <sgreenhusted@liquidweb.com>
2018-02-18 23:00:09 -0500

    basic employee salary by date range function

--- models/employee.go
+++ models/employee.go
@@ -18,3 +18,55 @@ type Employee struct {
 func (Employee) TableName() string {
 	return "employees"
 }
+
+func (this Employee) GetSalaryByTimeRange(from time.Time, to time.Time) (rangeSalary float64, err error) {
+	startWindow := from.Format("2006-01-02")
+	endWindow   := to.Format("2006-01-02")
+
+	db:= GetDb()
+
+	var salaries [] Salary
+	db.
+	Where(&Salary{EmployeeNumber: this.Number}).
+	Where(`(from_date <= ? and to_date >= ?),
+		or (from_date <= ? and to_date >= ?)
+		or (from_date >= ? and to_date <= ?)
+		or (from_date <= ? and to_date >= ?)`,
+		startWindow, startWindow,
+		startWindow, endWindow,
+		startWindow, endWindow,
+		endWindow, endWindow,
+	).
+	Find(&salaries)
+
+	var total float64
+
+	for _, salary := range salaries {
+		start := salary.StartDate
+		end   := salary.EndDate
+		if start.Equal(from) && end.Equal(to) {
+			//Easy Mode
+			total += salary.Salary
+		} else {
+			var intervalStart time.Time
+			var intervalStop time.Time
+			if start.After(from) {
+				intervalStart = start
+			} else {
+				intervalStart = from
+			}
+			if end.Before(to) {
+				intervalStop = end
+			} else {
+				intervalStop = to
+			}
+
+			interval := intervalStop.Sub(intervalStart)
+			intervalSalary := salary.DailyRate() * (interval.Hours() / 24)
+
+			total += intervalSalary
+		}
+	}
+
+	return total, nil
+}
--- sarge_test.go
+++ sarge_test.go
@@ -1,18 +1,76 @@
 package main_test
 
 import (
+	"github.com/jinzhu/gorm"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	. "github.com/ricecake/sarge/models"
+	mocket "github.com/selvatico/go-mocket"
 	"time"
 )
 
 var _ = Describe("Sarge", func() {
 	Describe("DB Model", func() {
+		BeforeEach(func() {
+			mocket.Catcher.Register()
+			db, err := gorm.Open(mocket.DRIVER_NAME, "A mocked connection")
+			if err != nil {
+				Fail("Failed to mock db!")
+			}
+			SetDb(db)
+		})
 		Describe("Department Struct", func() {
 		})
 		Describe("Employee Struct", func() {
-
+			var (
+				easyEmployee Employee
+			)
+			BeforeEach(func() {
+				mocket.Catcher.Reset()
+				easyEmployee = Employee{
+					Number:    5,
+					FirstName: "Sam",
+					LastName:  "Baker",
+					Gender:    "f",
+				}
+			})
+			Describe("GetSalaryByTimeRange", func() {
+				Context("Simple time ranges", func() {
+					It("Returns expected sum of quarterly salaries", func() {
+						mocket.Catcher.Attach([]*mocket.FakeResponse{
+							{
+								Once:    false,
+								Pattern: "SELECT * FROM \"salaries\"  WHERE",
+								Response: []map[string]interface{}{
+									{
+										"emp_no":    5,
+										"salary":    52000.00,
+										"from_date": time.Date(2009, time.January, 1, 0, 0, 0, 0, time.UTC),
+										"to_date":   time.Date(2010, time.January, 1, 0, 0, 0, 0, time.UTC),
+									},
+								},
+							},
+						})
+						Q1, _ := easyEmployee.GetSalaryByTimeRange(
+							time.Date(2009, time.January, 1, 0, 0, 0, 0, time.UTC),
+							time.Date(2009, time.April, 1, 0, 0, 0, 0, time.UTC),
+						)
+						Q2, _ := easyEmployee.GetSalaryByTimeRange(
+							time.Date(2009, time.April, 1, 0, 0, 0, 0, time.UTC),
+							time.Date(2009, time.July, 1, 0, 0, 0, 0, time.UTC),
+						)
+						Q3, _ := easyEmployee.GetSalaryByTimeRange(
+							time.Date(2009, time.July, 1, 0, 0, 0, 0, time.UTC),
+							time.Date(2009, time.October, 1, 0, 0, 0, 0, time.UTC),
+						)
+						Q4, _ := easyEmployee.GetSalaryByTimeRange(
+							time.Date(2009, time.October, 1, 0, 0, 0, 0, time.UTC),
+							time.Date(2010, time.January, 1, 0, 0, 0, 0, time.UTC),
+						)
+						Expect(Q1+Q2+Q3+Q4).To(Equal(52000.00))
+					})
+				})
+			})
 		})
 		Describe("Salary Struct", func() {
 			Describe("Basic Helpers", func() {
@@ -39,8 +97,8 @@ var _ = Describe("Sarge", func() {
 				It("Should Calculate salary duration", func() {
 					Expect(testSalary.Duration()).To(Equal(uint(365)))
 				})
-				It("Should Calculate correct daily rate", func(){
-					Expect(testSalary.DailyRate()).To(Equal(75000.00/365))
+				It("Should Calculate correct daily rate", func() {
+					Expect(testSalary.DailyRate()).To(Equal(75000.00 / 365))
 				})
 			})
 		})
